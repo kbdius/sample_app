1c045657721240b13952d1caccdec4ec6be714fc
Kurt Brown <kbrown@dius.com.au>
2013-05-22 14:35:28 +1000

    add signin / out

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name     "Kurt Brown2"
-    email    "kbrown@dius.com.au"
-    password "Password2"
-    password_confirmation "Password2"
+    name     "Test User"
+    email    "testuser@dius.com.au"
+    password "Password"
+    password_confirmation "Password"
   end
 end
